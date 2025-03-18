1b42316f2153d728a286d612bed451fc2cbc5439
Kowabunga <77234307+KowabungaOfficial@users.noreply.github.com>
2025-03-18 01:44:05 -0500

    Update sampler.cpp

--- src/sampler.cpp
+++ src/sampler.cpp
@@ -1,6 +1,6 @@
 #include "sampler.hpp"
 
-namespace vkBasalt
+namespace vkPost
 {
     VkSampler createSampler(LogicalDevice* pLogicalDevice)
     {
@@ -124,4 +124,4 @@ namespace vkBasalt
                 return;
         }
     }
-} // namespace vkBasalt
+} // namespace vkPost
