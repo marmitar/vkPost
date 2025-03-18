2f27dfb79dfafa1d91a78b38b7c0b8a49567b508
Kowabunga <77234307+KowabungaOfficial@users.noreply.github.com>
2025-03-18 01:34:39 -0500

    Update keyboard_input.cpp

--- src/keyboard_input.cpp
+++ src/keyboard_input.cpp
@@ -3,19 +3,19 @@
 #include "logger.hpp"
 
 // TODO build without X11
-#ifndef VKBASALT_X11
-#define VKBASALT_X11 1
+#ifndef VKPOST_X11
+#define VKPOST_X11 1
 #endif
 
-#if VKBASALT_X11
+#if VKPOST_X11
 #include "keyboard_input_x11.hpp"
 #endif
 
-namespace vkBasalt
+namespace vkPost
 {
     uint32_t convertToKeySym(std::string key)
     {
-#if VKBASALT_X11
+#if VKPOST_X11
         return convertToKeySymX11(key);
 #endif
         return 0u;
@@ -23,9 +23,9 @@ namespace vkBasalt
 
     bool isKeyPressed(uint32_t ks)
     {
-#if VKBASALT_X11
+#if VKPOST_X11
         return isKeyPressedX11(ks);
 #endif
         return false;
     }
-} // namespace vkBasalt
+} // namespace vkPost
